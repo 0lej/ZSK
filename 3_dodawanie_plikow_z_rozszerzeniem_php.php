26cf4ad292fcefea6c53e89ab88f6af192227686
0lej <90684678+0lej@users.noreply.github.com>
2021-09-14 12:12:56 +0200

    Create 3_dodawanie_plikow_z_rozszerzeniem_php.php
    added file with html

---
+++ 3_dodawanie_plikow_z_rozszerzeniem_php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>początek pliku</h3>
+    <?php
+
+    // include './3_1.php1'; //nie wywali fatal error
+
+    require './3_1.php'; //
+    ?>
+    <h3>koniec pliku</h3>
+  </body>
+</html>
