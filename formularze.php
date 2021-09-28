54cef496fbb37f30124174066129125348d8d13d
0lej <90684678+0lej@users.noreply.github.com>
2021-09-28 12:14:21 +0200

    Update formularze.php
    change formularze.php

--- formularze.php
+++ formularze.php
@@ -1,1 +1,15 @@
-
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>formularz</title>
+  </head>
+  <body>
+<h3>figury geometryczne</h3>
+    <form action="./4_script.php" method="post">
+      <input type="text" name="name" placeholder="Podaj Imię"><br><br>
+      <input type="radio" name="geometric_Figure" value="kwadrat" checked>kwadrat<br><br>
+      <input type="radio" name="geometric_Figure" value="prostokat">prostokąt<br><br>
+      <input type="submit" value="zatwierdz figurę">
+    </form>
+  </body>
+</html>
